ab745e93bda2cbfaedffa8c3f77391622948963c
Daniel Baker <dbaker@cita.utoronto.ca>
2019-03-19 12:24:18 -0400

    Expanded Integral

--- Lens_Mod.py
+++ Lens_Mod.py
@@ -98,7 +98,7 @@ def I_calc_indiv(tasks):
 		ints_tot=np.concatenate((ints_tot,z_list[i]))
 	ints_tot=np.unique(ints_tot)
 	for i in range(ints_tot.shape[0]//2):
-		I+=quadrature(igrand,ints_tot[::2][i],ints_tot[1::2][i],args=(x0,inc,sig,S_par,sheet,sheet_dl),epsrel=1.e-16)[0]
+		I+=quadrature(igrand,ints_tot[::2][i],ints_tot[1::2][i],args=(x0,inc,sig,S_par,sheet,sheet_dl))[0]
 	return(I)
 
 def I_calc_mpi(x,sheet,sheet_dl,S_par,zmax,zmin,inc,x0_crit,z_list,sig,pool):
